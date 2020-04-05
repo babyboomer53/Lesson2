e3842b1d73f4b194d49582e749e002a8fde9e3b1
Edgar Cole <edgarcolejr@gmail.com>
2020-04-04 19:49:07 -0700

    This version utilizes nested for-loops to modify the program's behavior to create four squares instead of one.

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -8,11 +8,14 @@ public class IntroToRobot {
 		Robot r2d2 = new Robot();
 		r2d2.penDown();
 		r2d2.setPenWidth(20);
-		r2d2.setSpeed(5);
-		for (int i = 0; i < 4; i++) {
-			r2d2.move(100);
-			r2d2.turn(90);
+		r2d2.setSpeed(4);
+		for (int countSquares = 1; countSquares <= 4; countSquares++) {
+			r2d2.setRandomPenColor();
+			for (int countSides = 1; countSides <= 4; countSides++) {
+				r2d2.move(100);
+				if (countSides < 4)
+					r2d2.turn(90);
+			}
 		}
-
 	}
 }
